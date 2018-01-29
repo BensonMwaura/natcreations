61914eada6c526cbf6db6dda7c47bf52f80553b3
Benson Mwaura <benson.cmwaura@gmail.com>
2018-01-29 17:42:08 +0300

    -- modified style: main | module: shared | component: about --

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MatButtonModule, MatMenuModule, MatCardModule, MatToolbarModule, MatTabsModule, MatIconModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeaderComponent } from './../core/components/header/header.component';
 import { LogoTextComponent } from './../core/components/logo-text/logo-text.component';
 import { BackLeafComponent } from '../core/components/back-leaf/back-leaf.component';
 import { ForeLeafComponent } from '../core/components/fore-leaf/fore-leaf.component';
@@ -22,7 +23,7 @@ import { ForeLeafComponent } from '../core/components/fore-leaf/fore-leaf.compon
     MatIconModule,
     FlexLayoutModule
   ],
-  declarations: [LogoTextComponent, BackLeafComponent, ForeLeafComponent],
+  declarations: [HeaderComponent, LogoTextComponent, BackLeafComponent, ForeLeafComponent],
   exports: [
     FormsModule,
     ReactiveFormsModule,
@@ -34,6 +35,7 @@ import { ForeLeafComponent } from '../core/components/fore-leaf/fore-leaf.compon
     MatTabsModule,
     MatIconModule,
     FlexLayoutModule,
+    HeaderComponent,
     LogoTextComponent,
     BackLeafComponent,
     ForeLeafComponent ]
