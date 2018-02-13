c70966b5d56050fdf3344a729918245b83cbf5ad
Benson Mwaura <benson.cmwaura@gmail.com>
2018-02-13 11:54:11 +0300

    -- modified style: main, component: products --

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from '../core/services/seo.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private seo: SeoService) { }
 
   ngOnInit() {
+    this.seo.generateTags({
+      title: 'Products',
+      description: 'Buy our custom designed products & accesories',
+      image: 'https://natcreo-390e9.firebaseapp.com/assets/img/Nature.png'
+    });
   }
 
 }
